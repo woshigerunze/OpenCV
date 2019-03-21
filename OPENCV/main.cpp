af669643226374d1e9a2005257434586eab6702a
woshigerunze <664279928@qq.com>
2019-03-21 15:49:22 +0800

    Initial Commit

---
+++ OPENCV/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  OPENCV
+//
+//  Created by 葛润泽 on 2019/3/21.
+//  Copyright © 2019 葛润泽. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
